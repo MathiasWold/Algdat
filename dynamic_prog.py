a3b527ef0b9db88eb2e7e74ed8598690291968b0
Mathias Wold <mathias.wold@hotmail.com>
2019-11-27 15:40:59 +0100

    Add 0/1 knapsack

--- dynamic_prog.py
+++ dynamic_prog.py
@@ -8,7 +8,7 @@ def LCS(x, y):
 
     n = len(x)
     m = len(y)
-    # each cell contains two objects; length and where it got it lenght from
+    # each cell contains two objects; length and where it got it length from
     # u = up, l = left, ul = upleft (diagonally)
     result = [[[0, "  "] for i in range(m+1)] for i in range(n+1)]
     lcs = ""
@@ -47,9 +47,13 @@ def LCS(x, y):
     print(f"LCS length: {result[n][m][0]}")
 
 
-x = list("stone")
-y = list("longest")
-LCS(x, y)
+def test1():
+    """ Test for LCS """
+    x = list("stone")
+    y = list("longest")
+    LCS(x, y)
+
+# test1()
 
 
 def rod_cut(prices, length):
@@ -62,25 +66,98 @@ def rod_cut(prices, length):
     result = [0] * (n + 1)
     cuts = [0] * (n + 1)
 
+    # for rods i of length 1 ... n
     for i in range(1, n + 1):
-        q = -sys.maxsize
+        # max value we can get for rods of length i
+        max_val = -sys.maxsize
+        # for cuts of lengths 1 ... i
         for j in range(1, i + 1):
-            if q < p[j] + result[i - j]:
-                q = p[j] + result[i - j]
+            # if max value is less than the value of a cut of j plus a cut of i - j --> set this sum as new max value for this i
+            if max_val < prices[j] + result[i - j]:
+                max_val = prices[j] + result[i - j]
+                # cuts[i] stores where we had to cut to get max value at a given rod with length i
                 cuts[i] = j
-        result[i] = q
+
+        # add the max value we can get for a rod of length i
+        result[i] = max_val
 
     print("Optimal value for rods of size equal to index i: ", result)
-    print("First cut to make at index i to construct an optimal solution:", 
-    cuts)
-    print(f"To get max value with a rod of length {n}, cut pieces of size ", end="")
+    print("First cut to make at index i to construct an optimal solution:",
+          cuts)
+    print(
+        f"To get max value with a rod of length {n}, cut pieces of size ", end="")
     while n > 0:
         print(cuts[n], end=", ")
         n = n - cuts[n]
     print(f"which gives a total value of {result[-1]}")
 
 
-# prices for selling rods of length equal to index of p
-p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-rod_cut(p, 10)
-rod_cut(p, 7)
+def test2():
+    """ Test for roc_cut """
+    # prices for selling rods of length equal to index of p
+    p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    rod_cut(p, 10)
+    rod_cut(p, 7)
+
+# test2()
+
+
+class Item:
+    """ Class for an item with given weight and value, to be used in the 0/1 knapsack problem """
+
+    def __init__(self, weight, value):
+        self.weight = weight
+        self.value = value
+
+
+def binary_knapsack(items, W):
+    """ Solves the 0/1 knapsack problem with a bottom-up approach using dynamic programming """
+
+    # adding a new "blank" item in front of the original items, to be used for comparison with the first item in the loops below
+    items = [Item(0, 0)] + items
+    # n items, W is max weight for knapsack
+    n = len(items)
+
+    # K holds the max value we can earn by including items 1 ... n with a max weight 0 ... W
+    # This is computed and stored in K[i][j] for item i and weight j
+    K = [[0 for i in range(W + 1)] for i in range(n)]
+
+    # i = row (item), j = column (weight)
+    for i in range(1, n):
+        for j in range(W + 1):
+            # x = exclude item i = max value for including up to item i - 1 at max weight j
+            x = K[i - 1][j]
+
+            # if weight j is less than weight of item i --> we do not have space for item i --> max value stays the same
+            if j < items[i].weight:
+                K[i][j] = x
+
+            # else weight j is larger than (or equal to) weight of item i
+            else:
+                # y = include item i = max value for including up to item i - 1 at max weight (j - weight of item i) plus value of item i
+                y = K[i - 1][j - items[i].weight] + items[i].value
+
+                # compare max values given by including or excluding item i at weight j, and set K[i][j] as the max of these values
+                K[i][j] = max(x, y)
+
+    # remove the first helper-row for the blank item before returning K
+    # max value we can get is in the bottom-right-most cell
+    return K[1:]
+
+
+def test3():
+    """ Test for binary knapsack """
+    a = Item(10, 60)
+    b = Item(20, 100)
+    c = Item(30, 120)
+
+    items = [a, b, c]
+    max_weight = 50
+    K = binary_knapsack(items, max_weight)
+    for row in K:
+        print(row)
+
+    print(f"Max value: {K[len(items) - 1][max_weight]}")
+
+
+test3()
